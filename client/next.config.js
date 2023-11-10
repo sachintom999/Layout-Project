83de4b3e07552834e8cece1caf87bcd81de18b4b
Prashanth BR <prashanthrepo@gmail.com>
2023-11-10 22:47:53 +0530

    next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
